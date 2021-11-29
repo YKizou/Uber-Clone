ba6697b5ac5bf41e1ba21fae0597dfdbf9751c1b
Youssef KIZOU <youssefkizou@air-de-youssef.home>
2021-11-29 17:14:15 +0100

    initial commit

---
+++ pages/components/Map.js
@@ -0,0 +1,66 @@
+import {useEffect, useState} from 'react'
+import tw from 'tailwind-styled-components'
+import mapboxgl from '!mapbox-gl'
+import * as turf from '@turf/turf'
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidGFvdXlha2l6IiwiYSI6ImNrd2M5bDFleTA5NGIybm4xZ2IwdmV0bTgifQ.z2SR41j5jR6y0e1-k26VFg';
+
+
+const Map = (props) => {
+
+    const [distance, setDistance] = useState('')
+
+    useEffect(() => {
+        console.log("props", props)
+        const pickupCoord = props.pickupCoordinate
+        const dropoffCoord = props.dropoffCoordinate
+        console.log("pickupCoord", pickupCoord)
+        console.log("dropoffCoord", dropoffCoord)
+
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+            center: [-96, 37.8],
+            zoom: 3,
+        })
+
+        if(pickupCoord && dropoffCoord){
+            addToMap(map, pickupCoord, dropoffCoord)
+        }
+
+    },)
+    
+
+    const addToMap = (map, pickupCoord, dropoffCoord) => {
+        // Create a default Marker and add it to the map.
+        const marker1 = new mapboxgl.Marker()
+        .setLngLat(pickupCoord)
+        .addTo(map);
+
+        // Create a default Marker and add it to the map.
+        const marker2 = new mapboxgl.Marker()
+        .setLngLat(dropoffCoord)
+        .addTo(map);
+
+        // Add padding to the map to visualize correctly the markers
+        map.fitBounds([pickupCoord, dropoffCoord], 
+            {padding: 100 
+        });
+
+    }
+    
+
+    return  (
+        <Wrapper id='map'></Wrapper>
+    )
+
+
+}
+
+export default Map
+
+const Wrapper = tw.div`
+flex flex-1  
+
+`
